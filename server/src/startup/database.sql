f4e019cbd51833be630e2ce90b7de83aef3b2673
Jason Wong <wongjason415@gmail.com>
2023-07-12 14:53:45 -0700

    initial database setup

---
+++ server/src/startup/database.sql
@@ -0,0 +1,19 @@
+CREATE TABLE stuffJotterUsers (
+    user_id SERIAL PRIMARY KEY,
+    user_username VARCHAR (30) NOT NULL,
+    user_password VARCHAR (100) NOT NULL,
+    user_email VARCHAR (255) NOT NULL,
+    user_role VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE itemList (
+    item_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    place VARCHAR NOT NULL,
+    item VARCHAR NOT NULL,
+    item_quantity INT NOT NULL,
+    item_description TEXT NOT NULL,
+    item_price NUMERIC(15,4) NOT NULL,
+    item_purchase_date VARCHAR (50) NOT NULL,
+    item_image_path VARCHAR (255) NOT NULL
+);
