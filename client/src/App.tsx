5d288c096252c5c04834c0f5a93dc1a45416089b
Jason Wong <wongjason415@gmail.com>
2023-08-02 15:51:52 -0700

    commit initial landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import Dashboard from "./pages/Dashboard/index";
 import LocationCardEdit from "./pages/Dashboard/LocationCardEdit";
 import NoPage from "./pages/NoPage";
@@ -11,7 +12,7 @@ export default function App() {
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
       <Routes>
-        <Route path="/" element />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route
           path="/dashboard/edit/:location"
