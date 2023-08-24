b0f5483ee8acadb49734f2724a16c05e7dd1e118
Jason Wong <wongjason415@gmail.com>
2023-08-23 22:12:57 -0700

    add server authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,21 @@ import express, { Application } from "express";
 import path from "path";
 import cors from "cors";
 import * as dotenv from "dotenv";
+const { auth } = require("express-oauth2-jwt-bearer");
 
 dotenv.config();
+
 const app: Application = express();
+
 app.use(cors());
+
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  tokenSigningAlg: "RS256",
+});
+app.use(jwtCheck);
+
 app.use(express.json({ limit: 52428800 }));
 //serve static files in dev
 app.use(express.static("testupload"));
@@ -14,4 +25,5 @@ app.use("/images/", express.static(path.join(__dirname, "../testupload")));
 require("./startup/routes")(app);
 
 const port: String = process.env.PORT || "5000";
+
 app.listen(port, () => console.log(`[server]: Sever running on port: ${port}`));
