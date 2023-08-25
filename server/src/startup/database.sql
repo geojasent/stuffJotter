ad795a208826046ff7f365e5694d4a3942783d5e
Jason Wong <wongjason415@gmail.com>
2023-08-24 22:14:55 -0700

    add user identifier column to database for authentication

--- server/src/startup/database.sql
+++ server/src/startup/database.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS stuffjotterusers (
     user_id SERIAL PRIMARY KEY,
+    user VARCHAR (50) NOT NULL,
     user_username VARCHAR (30) NOT NULL,
     user_password VARCHAR (100) NOT NULL,
     user_email VARCHAR (255) NOT NULL,
@@ -9,12 +10,14 @@ CREATE TABLE IF NOT EXISTS stuffjotterusers (
 CREATE TABLE IF NOT EXISTS userplaces (
     id SERIAL PRIMARY KEY,
     user_id INT,
+    user VARCHAR (50) NOT NULL,
     place VARCHAR NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS itemlist (
     item_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
+    user VARCHAR (50) NOT NULL,
     place VARCHAR NOT NULL,
     item VARCHAR NOT NULL,
     item_quantity INT NOT NULL,
@@ -29,6 +32,7 @@ CREATE TABLE IF NOT EXISTS stored_file_path (
     file_id SERIAL PRIMARY KEY,
     item_id INT NOT NULL UNIQUE,
     user_id INT NOT NULL,
+    user VARCHAR (50) NOT NULL,
     place VARCHAR NOT NULL,
     dashboard BOOLEAN NOT NULL,
     file_path VARCHAR NOT NULL,
