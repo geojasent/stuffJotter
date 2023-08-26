b3d8396a425f3814816037d63c097db2ac979839
Jason Wong <wongjason415@gmail.com>
2023-08-25 22:18:46 -0700

    initial auth0 client authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,27 +3,33 @@ import ResponsiveAppBar from "./components/Navbar";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import Dashboard from "./pages/Dashboard/index";
 import LocationCardEdit from "./pages/Dashboard/LocationCardEdit";
+import Login from "./pages/Login/Login";
 import NoPage from "./pages/NoPage";
+import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
 import "./App.css";
 
 export default function App() {
   document.title = "StuffJotter";
+
   return (
     <BrowserRouter>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
-      <ResponsiveAppBar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route
-          path="/dashboard/edit/:location"
-          element={<LocationCardEdit />}
-        />
-        <Route path="/*" element={<NoPage />} />
-      </Routes>
+      <Auth0ProviderWithNavigate>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard/edit/:location"
+            element={<LocationCardEdit />}
+          />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   );
 }
